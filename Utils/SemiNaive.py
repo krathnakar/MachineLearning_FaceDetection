97ad855a447d12f23d6059f71aca1f66d4fdcd0d
rathnakar14 <rathnakar.kallamadi@gmail.com>
2016-02-04 20:21:29 -0600

    c1 completed

--- Utils/SemiNaive.py
+++ Utils/SemiNaive.py
@@ -6,7 +6,8 @@ class SemiNaive:
         cutoff = numCutOff
         face = np.array([[1,1,1],[1 ,0 ,1],[0 ,1 ,1],[1 ,1 ,1]])
         nonface = np.array([[1,1,1,0],[0,1,0,1],[0,1,1,0]]).T
-        #print face.T
+        print face
+        print nonface
         CTClass1 = occArrayFunc(self, face, cutoff, group)
         CPTClass1 = CoPairTabClass(self, face, cutoff, group)
         CTClass2 = occArrayFunc(self, nonface, cutoff, group)
@@ -42,52 +43,59 @@ def CoPairTabClass(self, face, cutoff, group):
         for j in range(pixels):
             for each in face[0:cutoff]:#restricting to Cutoff->2 samples
                 coPairTabClass[i][j][each[i]][each[j]]+=1
+    #print "one"
+    #print coPairTabClass
     return coPairTabClass
 
 
 def FindProb2(self, CPTClass1, CPTClass2, CTClass1, CTClass2, group):
     print CPTClass1.shape
     epsilon = np.finfo(np.float).eps
+
     C1 = np.zeros((CPTClass1.shape[0],CPTClass1.shape[1]))
-    m = CPTClass1.shape[2] - 1
-    n = CPTClass1.shape[3] - 1
-    print m
-    print n
-    for i in range(CPTClass1.shape[0]):
+    res2 = 0.0
 
-        for j in range(CPTClass1.shape[1]):
-
+    for i in range(len(CPTClass1[0])):
+        #res2 = 0.0
+        for j in range(len(CPTClass1[1])):
+            res2 = 0.0
             pxixj = 0.0
             pxixjw1 = 0.0
             pxixjw2 = 0.0
             pxiw1 = 0.0
             pxjw1 = 0.0
-            res = 0.0
             pxiw2 = 0.0
             pxjw2 = 0.0
-            for m in range(CPTClass1.shape[2]):
+            res = 0.0
+            for each in range(CPTClass1.shape[2]):
+                res2 = 0.0
+                for each2 in range(CPTClass1.shape[3]):
+                    #res=0.0
+                    ri1 = CTClass1[i][each]
+                    rj1 = CTClass1[j][each2]
+                    ri2 = CTClass2[i][each]
+                    rj2 = CTClass2[j][each2]
 
-                ri1 = CTClass1[i][m]
-                rj1 = CTClass1[j][m]
-                ri2 = CTClass2[i][m]
-                rj2 = CTClass2[j][m]
+                #for each2 in range(CPTClass1.shape[3]):
+                    q1 = CPTClass1[i][j][each][each2]
+                    q2 = CPTClass2[i][j][each][each2]
+                    pxixj = (q1 + q2) / (group + group)
+                    pxixjw1 = max(q1 / group, epsilon)
+                    pxixjw2 = max(q2 / group, epsilon)
+                    pxiw1 = ri1/group
+                    pxjw1 = rj1/group
+                    pxiw2 = ri2/group
+                    pxjw2 = rj2/group
 
-                for n in range(CPTClass1.shape[3]):
-                    q1 = CPTClass1[i][j][m][n]
-                    q2 = CPTClass2[i][j][m][n]
-                    pxixj += (q1 + q2) / (group + group)
-                    pxixjw1 += q1 / group
-                    pxixjw2 += max(q2 / group, epsilon)
-                pxiw1 += ri1/group
-                pxjw1 += rj1/group
-                pxiw2 += max(ri2/group, epsilon)
-                pxjw2 += max(rj2/group, epsilon)
+                    b_num = pxiw1*pxjw1
+                    b_den = max(pxiw2*pxjw2, epsilon)
 
-            a= np.log(max(pxixjw1/pxixjw2,epsilon))
-            b= np.log(max((pxiw1*pxjw1)/(pxiw2*pxjw2),epsilon))
-            res += pxixj * abs(a - b)
-            res2 = res
-            C1[i][j] = res2
+                    a = np.log(pxixjw1/pxixjw2)
+                    b = np.log(max(b_num/b_den, epsilon))
+                    res += pxixj * abs(a - b)
+                res2 += res
+            #res2 = res
+            C1[i][j] = float("{0:.3f}".format(res2))
     print C1
 
 
